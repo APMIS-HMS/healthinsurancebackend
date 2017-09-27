d2acf6293e19ff18ae9e2e144ab0bfbcb528d2c1
Simdi <ezeanietochisimdi@gmail.com>
2017-09-27 16:05:37 +0100

    Updated

--- src/viewmodels/premium.js
+++ src/viewmodels/premium.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 require('mongoose-money'); // require mongoose-money before Schema
 const Schema = mongoose.Schema;
@@ -7,7 +6,7 @@ const Money = require('moneyjs');
 const premiumSchema = new Schema({
     amount: { type: Schema.Types.Money, required: true, index: true },
     duration: { type: Number, required: true },
-    durationType: { type: Schema.Types.String, required: true },
+    unit: { type: Schema.Types.String, required: true },
     durationInDay: { type: Number, required: true },
     createdAt: { type: Date, 'default': Date.now },
     updatedAt: { type: Date, 'default': Date.now }
