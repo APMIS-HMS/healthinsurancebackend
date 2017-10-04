c90a04754a05be0c4fc6ac80893c7db67a57c49a
Chisimdi Ezeanieto <ezeanietochisimdi@gmail.com>
2017-10-04 18:58:59 +0100

    Updated

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -2,20 +2,20 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-  const userType = require('../models/user-types.model');
-  const mongoose = require('mongoose');
-  const Schema = mongoose.Schema;
+module.exports = function(app) {
+    const mongooseClient = app.get('mongooseClient');
+    const userType = require('../models/user-types.model');
+    const mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
 
-  const users = new mongooseClient.Schema({
-    email: { type: String, unique: true },
-    password: { type: String },
-    userType: { type: Schema.Types.Mixed },
-    roles: [{ type: Schema.Types.Mixed }],
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-  });
+    const users = new mongooseClient.Schema({
+        email: { type: String, unique: true },
+        password: { type: String },
+        userType: { type: Schema.Types.Mixed }, // Changes
+        roles: [{ type: Schema.Types.Mixed }],
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now }
+    });
 
-  return mongooseClient.model('users', users);
+    return mongooseClient.model('users', users);
 };
