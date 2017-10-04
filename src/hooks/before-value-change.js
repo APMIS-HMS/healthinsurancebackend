24efe1e0dc3f5b30a1731ee170e0f1977c83a0bb
Chisimdi Ezeanieto <ezeanietochisimdi@gmail.com>
2017-10-04 11:57:22 +0100

    Updated

--- src/hooks/before-value-change.js
+++ src/hooks/before-value-change.js
@@ -2,21 +2,21 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-  return function beforeValueChange(hook) {
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
-    
-    if (!hook.path.toString().contain("audit-trays")) {
-      this.find({
-        query: { _id: hook.data._id }
-      }).then(payload => {
-        if (payload.data.length != 0) {
-          hook.data.beforeValue = payload.data[0];
-        }
-      });
-    }
+module.exports = function(options = {}) { // eslint-disable-line no-unused-vars
+    return function beforeValueChange(hook) {
+        // Hooks can either return nothing or a promise
+        // that resolves with the `hook` object for asynchronous operations
 
-    return Promise.resolve(hook);
-  };
+        if (!hook.path.toString().contain("audit-trays")) {
+            this.find({
+                query: { _id: hook.data._id }
+            }).then(payload => {
+                if (payload.data.length != 0) {
+                    hook.data.beforeValue = payload.data[0];
+                }
+            });
+        }
+
+        return Promise.resolve(hook);
+    };
 };
