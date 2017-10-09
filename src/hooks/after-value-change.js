4216bf3f2be4ff184f84d232a779d9209798b37b
farquardsolve <fakuadesegun@live.com>
2017-10-09 09:17:18 +0100

    update

--- src/hooks/after-value-change.js
+++ src/hooks/after-value-change.js
@@ -6,16 +6,19 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     // that resolves with the `hook` object for asynchronous operations
     //console.log(hook.data.user);
     if (hook.path.toString() != "audit-trays") {
-      var tray = {
-        "user": hook.params.user,
-        "operation": hook.method,
-        "changes": {
-          "before": hook.data.beforeValue,
-          "after": hook.data
-        }
-      };
-      delete hook.beforeValue;
-      hook.app.service("audit-trays").create(tray);
+
+      if (hook.data.beforeValue != undefined) {
+        var tray = {
+          "user": hook.params.user,
+          "operation": hook.method,
+          "changes": {
+            "before": hook.data.beforeValue,
+            "after": hook.data
+          }
+        };
+        delete hook.data.beforeValue;
+        hook.app.service("audit-trays").create(tray);
+      }
     }
 
     return Promise.resolve(hook);
