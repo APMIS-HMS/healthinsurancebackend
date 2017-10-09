52a767f635cf2722ae812615273541e64a836e4f
Chisimdi Ezeanieto <ezeanietochisimdi@gmail.com>
2017-10-09 09:16:04 +0100

    Updated

--- src/hooks/after-value-change.js
+++ src/hooks/after-value-change.js
@@ -1,23 +1,23 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-  return function afterValueChange(hook) {
-    // Hooks can either return nothing or a promise
-    // that resolves with the `hook` object for asynchronous operations
-    //console.log(hook.data.user);
-    if (hook.path.toString() != "audit-trays") {
-      var tray = {
-        "user": hook.params.user,
-        "operation": hook.method,
-        "changes": {
-          "before": hook.data.beforeValue,
-          "after": hook.data
+module.exports = function(options = {}) { // eslint-disable-line no-unused-vars
+    return function afterValueChange(hook) {
+        // Hooks can either return nothing or a promise
+        // that resolves with the `hook` object for asynchronous operations
+        //console.log(hook.data.user);
+        if (hook.path.toString() != "audit-trays") {
+            var tray = {
+                "user": hook.params.user,
+                "operation": hook.method,
+                "changes": {
+                    "before": hook.data.beforeValue,
+                    "after": hook.data
+                }
+            };
+            delete hook.beforeValue;
+            hook.app.service("audit-trays").create(tray);
         }
-      };
-      delete hook.beforeValue;
-      hook.app.service("audit-trays").create(tray);
-    }
 
-    return Promise.resolve(hook);
-  };
+        return Promise.resolve(hook);
+    };
 };
