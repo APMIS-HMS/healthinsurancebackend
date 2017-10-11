9b3fca3421fb33e13bb9ec558b1be0c571ce91bb
farquardsolve <fakuadesegun@live.com>
2017-10-11 16:37:52 +0100

    update

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -78,6 +78,7 @@ var thumbnail = new Thumbnail('./public/uploads/image', './public/uploads/thumbn
   });
 
   app.post('/lashma-beneficiaries', beneficiary(app));
+  app.put('/lashma-beneficiaries', beneficiary(app));
   //app.use(notFound());
   // app.use(logger(app));
   // app.use(handler());
