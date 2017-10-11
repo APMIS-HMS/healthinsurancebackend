1e36172aad0117ec5ffd2d39dfbb9b3e545c84f7
sabaoth <sunday@apmis.ng>
2017-10-11 09:50:57 +0100

    updates

--- src/viewmodels/beneficiary-policy-model.js
+++ src/viewmodels/beneficiary-policy-model.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const accessibilitySchema = new Schema({
+const beneficiaryPolicySchema = new Schema({
     beneficiary: { type: Schema.Types.Mixed, required: true },
     isActive: { type: Schema.Types.Boolean, default: false },
+    policyId: { type: String, required: true },
     createdAt: { type: Date, 'default': Date.now },
     updatedAt: { type: Date, 'default': Date.now }
 })
-module.exports = accessibilitySchema;
+module.exports = beneficiaryPolicySchema;
