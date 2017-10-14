589ccee68fb3a21689518d59f6f3e670c7e13044
farquardsolve <fakuadesegun@live.com>
2017-10-14 22:50:33 +0100

    update

--- src/models/claim.model.js
+++ src/models/claim.model.js
@@ -6,24 +6,22 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const claimStatusModel = require('../viewmodels/claim-status-model');
   const claimAlertMessage = require('../viewmodels/claim-message-model');
+  const claimDoc = require('../viewmodels/claim-document-model');
   const { Schema } = mongooseClient;
   const claim = new Schema({
-    enrolleeName: { type: String, required: true },
-    enrolleeNumber: { type: String, required: true },
+    beneficialDetail:{ type: Schema.Types.Mixed, required: true }, 
+    checkedinDetail:{ type: Schema.Types.Mixed, required: true }, 
+    documentation:[claimDoc],
+    CurentClaimStatus:{ type: Schema.Types.Mixed, required: true }, 
+    approval:{ type: Schema.Types.Mixed, required: true }, 
     medicalPersonelName: { type: String, required: true },
     medicalPersonelShortName: { type: String, required: true },
-    authorizationId: { type: String, required: false },
-    claimCreatedAt:{ type: Date, required: true },
-    diagnosisCode:{ type: String, required: false },
-    investigationCode:{ type: String, required: false },
-    therapyCode:{ type: String, required: false },
-    claimStatus:claimStatusModel,
-    totalAmount:{ type: Number, default: 0 },
+    authorizationCode: { type: String, required: false },
+    costingApprovalDocumentation:{ type: Number, default: 0 },
     paymentStatus:{ type: Schema.Types.Mixed, required: false },
-    claimClass:{ type: Schema.Types.Mixed, required: false },
-    ApprovedDocumentation:{ type: Schema.Types.Mixed, required: false },
+    approvedDocumentation:[claimDoc],
     notificationMessage:claimAlertMessage,
-    createdAt: { type: Date, default: Date.now },
+    dateClaimCreated: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
 
