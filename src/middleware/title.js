1ea3f01af0f5681ec40d14f39a755034f2b5db8b
farquardsolve <fakuadesegun@live.com>
2017-10-19 16:03:24 +0100

    update

--- src/middleware/title.js
+++ src/middleware/title.js
@@ -2,7 +2,6 @@
 module.exports = function (app) {
     return function (req, res, next) {
         // Perform actions
-        
         app.service('titles').find({}).then(payload => {
                 res.send(payload);
             }).
