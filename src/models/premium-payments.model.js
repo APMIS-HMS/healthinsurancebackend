eff69ecb0ed7808f59bf8e0aff159665bb0ac075
Chisimdi Ezeanieto <ezeanietochisimdi@gmail.com>
2017-10-25 12:03:56 +0100

    Updated policy with isPaid and turned policy property to an array in premium-payment

--- src/models/premium-payments.model.js
+++ src/models/premium-payments.model.js
@@ -8,12 +8,13 @@ module.exports = function(app) {
     const premiumPayments = new Schema({
         platformOwnerId: { type: Schema.Types.Mixed, required: true },
         reference: { type: Schema.Types.Mixed, required: true }, // Reference from paystack.
-        policy: { type: Schema.Types.Mixed, required: true },
+        policy: [{ type: Schema.Types.Mixed, required: true }], // Array of policy ids.
         paidBy: { type: Schema.Types.Mixed, required: true },
         requestedAmount: { type: Number, required: true }, // Amount to be paid from our end.
         amountPaid: { type: Number, required: true }, // Amount paid at paystack's end.
         isActive: { type: Boolean, default: false }, // This will be set to true if payment is confirmed.
         paystackResponse: { type: Schema.Types.Mixed, required: false }, // This is the response from paystack if payment has been made.
+        paymentType: { type: Schema.Types.Mixed, required: true },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
     });
