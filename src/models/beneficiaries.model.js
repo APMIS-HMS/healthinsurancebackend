0b8afa22f870ab535a6ec694714baf0cdb6f127f
starday01 <sunday@apmis.ng>
2017-10-27 00:16:49 +0100

    updates

--- src/models/beneficiaries.model.js
+++ src/models/beneficiaries.model.js
@@ -9,9 +9,9 @@ module.exports = function (app) {
     stateID: { type: String, required: false }, // lasraid
     platformOwnerNumber: { type: String, required: false }, //lashmaid
     numberOfUnderAge: { type: Number, default: 0 },
-    personId: { type: Schema.Types.Mixed, required: true },
-    platformOwnerId: { type: Schema.Types.Mixed, required: true }, // platform owner object
-    isActive:{ type:Schema.Types.Boolean, default: false},
+    personId: { type: Schema.Types.ObjectId, required: true },
+    platformOwnerId: { type: Schema.Types.ObjectId, required: true }, // platform owner object
+    isActive: { type: Schema.Types.Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
