fb1eeb415c02a4e2d3cd5c39ed7e2de010844fd8
Chisimdi Ezeanieto <ezeanietochisimdi@gmail.com>
2017-10-27 07:33:56 +0100

    Updated

--- src/models/beneficiaries.model.js
+++ src/models/beneficiaries.model.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
         numberOfUnderAge: { type: Number, default: 0 },
         personId: { type: Schema.Types.Mixed, required: true },
         platformOwnerId: { type: Schema.Types.Mixed, required: true }, // platform owner object
-        isCapitationPaid: { type: Schema.Types.Boolean, default: false },
+        isCapitationPaid: { type: Schema.Types.Boolean, default: false }, // This is to pay capitation
         isActive: { type: Schema.Types.Boolean, default: false },
         createdAt: { type: Date, default: Date.now },
         updatedAt: { type: Date, default: Date.now }
