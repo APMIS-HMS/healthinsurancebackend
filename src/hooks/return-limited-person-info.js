1feca7a38112e5e137ddcd58637506e530acf1f4
farquardsolve <fakuadesegun@live.com>
2017-11-02 18:06:17 +0100

    update

--- src/hooks/return-limited-person-info.js
+++ src/hooks/return-limited-person-info.js
@@ -32,32 +32,34 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
       return Promise.resolve(hook);
     } else {
       if (hook.params.user === undefined) {
-        if (hook.result.data.length > 0) {
-          let person = hook.result.data[0];
-          hook.result.data.forEach(person => {
-            delete person.email;
-            delete person._id;
-            delete person.gender;
-            delete person.firstName;
-            delete person.lastName;
-            delete person.otherNames;
-            delete person.mothersMaidenName;
-            delete person.phoneNumber;
-            delete person.title;
-            delete person.platformOwnerId;
-            delete person.stateOfOrigin;
-            delete person.isActive;
-            delete person.createdAt;
-            delete person.dateOfBirth;
-            delete person.homeAddress;
-            delete person.lgaOfOrigin;
-            delete person.maritalStatus;
-            delete person.platformOnwerId;
-            delete person.employees;
-            delete person.nextOfKin;
-            delete person.maritalStatus;
-          });
-          return Promise.resolve(hook);
+        if (hook.result.data != undefined) {
+          if (hook.result.data.length > 0) {
+            let person = hook.result.data[0];
+            hook.result.data.forEach(person => {
+              delete person.email;
+              delete person._id;
+              delete person.gender;
+              delete person.firstName;
+              delete person.lastName;
+              delete person.otherNames;
+              delete person.mothersMaidenName;
+              delete person.phoneNumber;
+              delete person.title;
+              delete person.platformOwnerId;
+              delete person.stateOfOrigin;
+              delete person.isActive;
+              delete person.createdAt;
+              delete person.dateOfBirth;
+              delete person.homeAddress;
+              delete person.lgaOfOrigin;
+              delete person.maritalStatus;
+              delete person.platformOnwerId;
+              delete person.employees;
+              delete person.nextOfKin;
+              delete person.maritalStatus;
+            });
+            return Promise.resolve(hook);
+          }
         }
       }
     }
--- src/middleware/create_beneficiary_api.js
+++ src/middleware/create_beneficiary_api.js
@@ -86,118 +86,192 @@ function aphaformator() {
 
 module.exports = function (app) {
     return function (req, res, next) {
+        var errorMessage = {
+            "State": "Failed",
+            "Transaction": "Creating Beneficiary"
+        };
         console.log("POST");
         var counter = 0;
         var persons = [];
         var beneficiaries = [];
         let principal = req.body.principal
-        //console.log(principal);
-        app.service('people').create(principal).then(person => {
-            var beneficiaryDetails = {
-                "numberOfUnderAge": req.body.principal.numberOfUnderAge,
-                "platformOwnerId": req.body.principal.platformOwnerId
-            };
-            beneficiaryDetails.personId = person;
-            //console.log(beneficiaryDetails)
+        console.log(req.body.principal.homeAddress.state);
+        app.service('countries').find({
+            query:
+            {
+                'states.name': req.body.principal.homeAddress.state,
+                $select: { 'states.$': 1 }
+            }
+        }).then(country => {
+            if (country.data[0] != undefined) {
+                let state = country.data[0].states[0];
+                delete state.cities;
+                delete state.lga;
+                principal.homeAddress.state = state;
+                var lgaFiltered = country.data[0].states[0].lgs.filter(x => x.name.toLowerCase() === req.body.principal.homeAddress.lga.toLowerCase());
+                if (lgaFiltered[0] != undefined) {
+                    principal.homeAddress.lga = lgaFiltered[0];
+                    app.service('countries').find({
+                        query:
+                        {
+                            'states.name': req.body.principal.stateOfOrigin,
+                            $select: { 'states.$': 1 }
+                        }
+                    }).then(origin => {
+                        if (origin.data[0] != undefined) {
+                            let stateOfOrigin = origin.data[0].states[0];
+                            delete stateOfOrigin.cities;
+                            delete stateOfOrigin.lga;
+                            principal.stateOfOrigin = stateOfOrigin;
+                            var lgaOriginFiltered = origin.data[0].states[0].lgs.filter(x => x.name.toLowerCase() === req.body.principal.lgaOfOrigin.toLowerCase());
+                            if (lgaOriginFiltered[0] != undefined) {
+                                principal.lgaOfOrigin = lgaOriginFiltered[0];
+                                app.service('gender').find({
+                                    query:
+                                    {
+                                        'name': req.body.principal.gender
+                                    }
+                                }).then(gender => {
+                                    if (gender.data[0] != undefined) {
+                                        principal.gender = gender.data[0];
+                                        app.service('people').create(principal).then(person => {
+                                            var beneficiaryDetails = {
+                                                "numberOfUnderAge": req.body.principal.numberOfUnderAge,
+                                                "platformOwnerId": req.body.principal.platformOwnerId
+                                            };
+                                            beneficiaryDetails.personId = person;
+                                            //console.log(beneficiaryDetails)
+                    
+                                            app.service('beneficiaries').create(beneficiaryDetails).then(beneficiary => {
+                                                // console.log(beneficiary);
+                                                req.body.dependent.forEach(function (item) {
+                                                    app.service('people').create(item).then(person2 => {
+                                                        var beneficiaryDetailDependant = {
+                                                            "numberOfUnderAge": item.numberOfUnderAge,
+                                                            "platformOwnerId": item.platformOwnerId
+                                                        };
+                    
+                                                        beneficiaryDetailDependant.personId = person2;
+                                                        app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
+                                                            var beneficiary_policy = {
+                                                                "beneficiary": beneficiary2,
+                                                                "relationshipId": item.relationship
+                                                            };
+                    
+                                                            beneficiaries.push(beneficiary_policy);
+                                                            counter += 1;
+                    
+                                                            if (counter == req.body.dependent.length) {
+                                                                PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
+                                                            }
+                                                        })
+                    
+                                                        // generateLashmaID(app, item.platformOwnerId).then(result1 => {
+                                                        //     counter += 1;
+                                                        //     let lastVal1 = result1[1] + counter;
+                                                        //     let strLastVal1 = formatValue(lastVal1);
+                                                        //     let resultVal1 = result1[0] +"-"+ strLastVal1;
+                                                        //     beneficiaryDetailDependant.platformOwnerNumber = resultVal1;
+                                                        //     app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
+                                                        //         var beneficiary_policy = {
+                                                        //             "beneficiary": beneficiary2,
+                                                        //             "relationshipId": item.relationship
+                                                        //         };
+                    
+                                                        //         beneficiaries.push(beneficiary_policy);
+                    
+                                                        //         if (counter == req.body.dependent.length) {
+                                                        //             PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
+                                                        //         }
+                                                        //     })
+                                                        // });
+                                                    }, error => {
+                                                        res.send(error);
+                                                    }).catch(err => {
+                                                        res.send(err);
+                                                        next
+                                                    });
+                                                });
+                                            });
+                                            // generateLashmaID(app, req.body.principal.platformOwnerId).then(result => {
+                                            //     console.log(result);
+                                            //     var lastVal = result[1] + 1;
+                                            //     let strLastVal = formatValue(lastVal);
+                                            //     let resultVal = result[0] +"-"+ strLastVal;
+                                            //     beneficiaryDetails.platformOwnerNumber = resultVal;
+                                            //     app.service('beneficiaries').create(beneficiaryDetails).then(beneficiary => {
+                                            //         req.body.dependent.forEach(function (item) {
+                                            //             app.service('people').create(item).then(person2 => {
+                                            //                 var beneficiaryDetailDependant = {
+                                            //                     "numberOfUnderAge": item.numberOfUnderAge,
+                                            //                     "platformOwnerId": item.platformOwnerId
+                                            //                 };
+                                            //                 beneficiaryDetailDependant.personId = person2;
+                                            //                 return generateLashmaID(app, item.platformOwnerId).then(result1 => {
+                                            //                     counter += 1;
+                                            //                     let lastVal1 = result1[1] + counter;
+                                            //                     let strLastVal1 = formatValue(lastVal1);
+                                            //                     let resultVal1 = result1[0] +"-"+ strLastVal1;
+                                            //                     beneficiaryDetailDependant.platformOwnerNumber = resultVal1;
+                                            //                     app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
+                                            //                         var beneficiary_policy = {
+                                            //                             "beneficiary": beneficiary2,
+                                            //                             "relationshipId": item.relationship
+                                            //                         };
+                    
+                                            //                         beneficiaries.push(beneficiary_policy);
+                    
+                                            //                         if (counter == req.body.dependent.length) {
+                                            //                             PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
+                                            //                         }
+                                            //                     })
+                                            //                 });
+                                            //             }, error => {
+                                            //                 res.send(error);
+                                            //             }).catch(err => {
+                                            //                 res.send(err);
+                                            //                 next
+                                            //             });
+                                            //         });
+                                            //     })
+                                            // });
+                                        }, error => {
+                                            res.send(error);
+                                        }).catch(err => {
+                                            res.send(err);
+                                            next
+                                        });
+                                    } else {
+                                        errorMessage.Details = req.body.principal.gender + " donot exist as a Gender";
+                                        errorMessage.Time = new Date();
+                                        res.send(errorMessage);
+                                    }
 
-            app.service('beneficiaries').create(beneficiaryDetails).then(beneficiary => {
-                // console.log(beneficiary);
-                req.body.dependent.forEach(function (item) {
-                    app.service('people').create(item).then(person2 => {
-                        var beneficiaryDetailDependant = {
-                            "numberOfUnderAge": item.numberOfUnderAge,
-                            "platformOwnerId": item.platformOwnerId
-                        };
+                                })
 
-                        beneficiaryDetailDependant.personId = person2;
-                        app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
-                            var beneficiary_policy = {
-                                "beneficiary": beneficiary2,
-                                "relationshipId": item.relationship
-                            };
-
-                            beneficiaries.push(beneficiary_policy);
-                            counter += 1;
-
-                            if (counter == req.body.dependent.length) {
-                                PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
+                            } else {
+                                errorMessage.Details = req.body.principal.lgaOfOrigin + " donot exist as a LGA State of Origin";
+                                errorMessage.Time = new Date();
+                                res.send(errorMessage);
                             }
-                        })
-
-                        // generateLashmaID(app, item.platformOwnerId).then(result1 => {
-                        //     counter += 1;
-                        //     let lastVal1 = result1[1] + counter;
-                        //     let strLastVal1 = formatValue(lastVal1);
-                        //     let resultVal1 = result1[0] +"-"+ strLastVal1;
-                        //     beneficiaryDetailDependant.platformOwnerNumber = resultVal1;
-                        //     app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
-                        //         var beneficiary_policy = {
-                        //             "beneficiary": beneficiary2,
-                        //             "relationshipId": item.relationship
-                        //         };
-
-                        //         beneficiaries.push(beneficiary_policy);
-
-                        //         if (counter == req.body.dependent.length) {
-                        //             PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
-                        //         }
-                        //     })
-                        // });
-                    }, error => {
-                        res.send(error);
-                    }).catch(err => {
-                        res.send(err);
-                        next
-                    });
-                });
-            });
-            // generateLashmaID(app, req.body.principal.platformOwnerId).then(result => {
-            //     console.log(result);
-            //     var lastVal = result[1] + 1;
-            //     let strLastVal = formatValue(lastVal);
-            //     let resultVal = result[0] +"-"+ strLastVal;
-            //     beneficiaryDetails.platformOwnerNumber = resultVal;
-            //     app.service('beneficiaries').create(beneficiaryDetails).then(beneficiary => {
-            //         req.body.dependent.forEach(function (item) {
-            //             app.service('people').create(item).then(person2 => {
-            //                 var beneficiaryDetailDependant = {
-            //                     "numberOfUnderAge": item.numberOfUnderAge,
-            //                     "platformOwnerId": item.platformOwnerId
-            //                 };
-            //                 beneficiaryDetailDependant.personId = person2;
-            //                 return generateLashmaID(app, item.platformOwnerId).then(result1 => {
-            //                     counter += 1;
-            //                     let lastVal1 = result1[1] + counter;
-            //                     let strLastVal1 = formatValue(lastVal1);
-            //                     let resultVal1 = result1[0] +"-"+ strLastVal1;
-            //                     beneficiaryDetailDependant.platformOwnerNumber = resultVal1;
-            //                     app.service('beneficiaries').create(beneficiaryDetailDependant).then(beneficiary2 => {
-            //                         var beneficiary_policy = {
-            //                             "beneficiary": beneficiary2,
-            //                             "relationshipId": item.relationship
-            //                         };
-
-            //                         beneficiaries.push(beneficiary_policy);
-
-            //                         if (counter == req.body.dependent.length) {
-            //                             PolicyIDRecurtion(beneficiaries, beneficiary, req.body.policy, res, next, app)
-            //                         }
-            //                     })
-            //                 });
-            //             }, error => {
-            //                 res.send(error);
-            //             }).catch(err => {
-            //                 res.send(err);
-            //                 next
-            //             });
-            //         });
-            //     })
-            // });
-        }, error => {
-            res.send(error);
-        }).catch(err => {
-            res.send(err);
-            next
-        });
+                        } else {
+                            errorMessage.Details = req.body.principal.stateOfOrigin + " donot exist as a State of Origin";
+                            errorMessage.Time = new Date();
+                            res.send(errorMessage);
+                        }
+                    })
+                }
+                else {
+                    errorMessage.Details = req.body.principal.homeAddress.lga + " donot exist as a Residential LGA";
+                    errorMessage.Time = new Date();
+                    res.send(errorMessage);
+                }
+            }
+            else {
+                errorMessage.Details = req.body.principal.homeAddress.state + " donot exist as a Residential State";
+                errorMessage.Time = new Date();
+                res.send(errorMessage);
+            }
+        })
     };
 };
