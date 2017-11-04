7a113be2edde5d6a4ccdf093579f42154d8cae95
farquardsolve <fakuadesegun@live.com>
2017-11-04 02:45:02 +0100

    update

--- src/hooks/return-limited-facility-info.js
+++ src/hooks/return-limited-facility-info.js
@@ -30,31 +30,34 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
       return Promise.resolve(hook);
     } else {
       if (hook.params.user === undefined) {
-        if (hook.result.data.length > 0) {
-          let facility = hook.result.data[0];
-          hook.result.data.forEach(facility => {
-            delete facility.email;
-            delete facility._id;
-            delete facility.address;
-            delete facility.businessContact;
-            delete facility.itContact;
-            delete facility.employer;
-            delete facility.facilityType;
-            delete facility.hia;
-            delete facility.provider;
-            delete facility.isTokenVerified;
-            delete facility.createdAt;
-            delete facility.bankDetails;
-            delete facility.name;
-            delete facility.phoneNumber;
-            delete facility.platformOwnerId;
-            delete facility.isConfirmed;
-            delete facility.shortName;
-            delete facility.website;
-            delete facility.logo;
-          });
-          return Promise.resolve(hook);
+        if(hook.result.data!=undefined){
+          if (hook.result.data.length > 0) {
+            let facility = hook.result.data[0];
+            hook.result.data.forEach(facility => {
+              delete facility.email;
+              delete facility._id;
+              delete facility.address;
+              delete facility.businessContact;
+              delete facility.itContact;
+              delete facility.employer;
+              delete facility.facilityType;
+              delete facility.hia;
+              delete facility.provider;
+              delete facility.isTokenVerified;
+              delete facility.createdAt;
+              delete facility.bankDetails;
+              delete facility.name;
+              delete facility.phoneNumber;
+              delete facility.platformOwnerId;
+              delete facility.isConfirmed;
+              delete facility.shortName;
+              delete facility.website;
+              delete facility.logo;
+            });
+            return Promise.resolve(hook);
+          }
         }
+        
       }
     }
   };
