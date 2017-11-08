578750105586d21a903d2ff368afcdf06413b87f
farquardsolve <fakuadesegun@live.com>
2017-11-08 11:47:03 +0100

    update

--- src/hooks/policy-id.js
+++ src/hooks/policy-id.js
@@ -2,7 +2,7 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 const promise = [];
-
+var alphabeth = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","X","Y","Z"];
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function policyId(hook) {
     // Hooks can either return nothing or a promise
@@ -16,12 +16,14 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
       for (var i = payload[0].total + 1; i <= payload[0].total + payload.length; i++) {
         console.log(i);
         let formatedValue = ("00000" + i).slice(-5);
-        var formatedCounter = "P" + formatedValue + "-1";
+        var formatedCounter = "P" + formatedValue;
         hook.data.policyId = formatedCounter;
+        hook.data.principalPolicyId = formatedCounter+"-A";
         console.log(hook.data.policyId);
         if (i == payload[0].total + payload.length) {
           hook.data.dependantBeneficiaries.forEach(function (bPolicyId, index) {
-            bPolicyId.policyId = formatedCounter + "-" + index + 1;
+            let count = index + 2;
+            bPolicyId.policyId = formatedCounter + "-" + alphabeth[count];
           });
         }
       }
