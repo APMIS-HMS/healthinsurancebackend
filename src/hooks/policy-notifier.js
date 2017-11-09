7af185fc598f86a21ed002a2a602a07bd7da800d
farquardsolve <fakuadesegun@live.com>
2017-11-09 09:18:50 +0100

    update

--- src/hooks/policy-notifier.js
+++ src/hooks/policy-notifier.js
@@ -1,7 +1,7 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-function postNotification(user,hook) {
+function postNotification(user, hook) {
   var notifier = {
     "title": hook.method,
     "body": connection.user.firstName + " " + connection.user.lastName + " (" +
@@ -10,9 +10,10 @@ function postNotification(user,hook) {
     "policyId": data._id,
     "userType": user.userType
   };
-  console.log(notifier);
+  console.log("------About to save-------");
   hook.app.service("notifications").create(notifier).then(payload => {
-    console.log(payload);
+    //console.log(payload);
+    console.log("------saved-------");
   });
 }
 
@@ -21,30 +22,41 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function policyNotifier(hook) {
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
-    hook.app.service("policies").filter((data, connection, hook) => {
+    hook.app.service("policies").filter((data, connection) => {
       let hia = data.hiaId._id;
       let provider = data.providerId._id;
       let user = connection.user;
-      console.log(user.userType.name);
-      console.log(connection.user.facilityId._id + " ------FacilityId");
-      console.log(hia + " ---HIA");
-      console.log(provider + " ---provider");
       if (user.userType.name === 'Beneficiary') {
-        console.log("It is a beneficiary");
         //notify hia, provider
-        postNotification(user,hook);
+        //postNotification(user, hook);
         return data;
       } else if (user.userType.name === 'Health Insurance Agent') {
-        postNotification(user,hook);
+        console.log("------Insider HIA--------");
+        //postNotification(user,hook);
+        var notifier = {
+          "title": hook.method + "ed a Policy:"+data.policyId,
+          "body": connection.user.firstName + " " + connection.user.lastName + " (" +
+          connection.user.userType.name + ") made a/an " + hook.method + " on " +
+          data.policyId + " at around " + data.createdAt,
+          "policyId": data._id,
+          "userType": user.userType
+        };
+        console.log("------About to save-------");
+        hook.app.service("notifications").create(notifier).then(payload => {
+          console.log(payload);
+          console.log("------saved-------");
+          
+        });
         return data;
+
       } else {
-        postNotification(user,hook);
+        //postNotification(user, hook);
         return data;
       }
 
     });
 
 
-        return Promise.resolve(hook);
-    };
+    return Promise.resolve(hook);
+  };
 };
