280953a304ee604b05326c088a294df376572253
starday01 <sunday@apmis.ng>
2017-11-22 11:48:30 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -1,10 +1,5 @@
 'use strict';
-const options = {
-    apiKey: '2d4e0c15346ba6e810bcfd74ed300cb4042a000fba6effeadbcb775e538bce44',         // Use sandbox API key for sandbox development
-    username: 'sandbox',      // Use "sandbox" for sandbox development
-};
 
-const AfricasTalking = require('africastalking')(options);
 module.exports = function (app) {
     return function (req, res, next) {
         let sessionId = req.body.sessionId;
