d49c6de949921ccad2b5763c9c02480dd0809e70
starday01 <sunday@apmis.ng>
2017-11-22 08:39:56 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -10,25 +10,10 @@ module.exports = function (app) {
         let sessionId = req.body.sessionId;
         let phoneNumber = req.body.phoneNumber;
         let serviceCode = req.body.serviceCode;
-        // let text = req.body.text;
-
-        console.log(req.body);
-        // let my = Object.keys(req.body[0]);
-        res.send('CON' + serviceCode);
+        let text = req.body.text;
+        
+        res.send('CON '+req.body.phoneNumber);
         next;
-        // if (text == undefined || text == null) {
-        //     // This is the first request
-        //     let response = 'CON Welcome to APMIS Insurance USSG Gateway \n'
-        //     response += '1. Verify Registrationtion \n'
-        //     response += '2. Register \n'
-        //     response += '3. Get Policy \n'
-        //     response += '4. Pay Premium'
-        //     res.send(response);
-        //     next;
-        // }else{
-        //     res.send(serviceCode);
-        //     next;
-        // }
-    };
-}
+    }
+};
 
