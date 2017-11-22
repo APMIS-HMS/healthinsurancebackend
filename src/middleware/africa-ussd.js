d385c9e50c5976b193e1a9660c0377037211b15f
starday01 <sunday@apmis.ng>
2017-11-22 08:43:00 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -11,8 +11,8 @@ module.exports = function (app) {
         let phoneNumber = req.body.phoneNumber;
         let serviceCode = req.body.serviceCode;
         let text = req.body.text;
-        
-        res.send('CON '+req.body.phoneNumber);
+
+        res.send('CON '+req.body.text);
         next;
     }
 };
