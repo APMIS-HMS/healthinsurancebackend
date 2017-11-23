206ec2e73a9663077637a739eb733aa4c3a43304
starday01 <sunday@apmis.ng>
2017-11-23 08:09:33 +0100

    updates

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -280,50 +280,7 @@ module.exports = function() {
     let menu = new UssdMenu();
 
 
-    menu.startState({
-        run: () => {
-            // use menu.con() to send response without terminating session      
-            menu.con('Welcome. Choose option:' +
-                '\n1. Show Balance' +
-                '\n2. Buy Airtime');
-        },
-        // next object links to next state based on user input
-        next: {
-            '1': 'showBalance',
-            '2': 'buyAirtime'
-        }
-    });
-    
-    menu.state('showBalance', {
-        run: () => {
-            // fetch balance
-            fetchBalance(menu.args.phoneNumber).then(function(bal){
-                // use menu.end() to send response and terminate session
-                menu.end('Your balance is KES ' + bal);
-            });
-        }
-    });
-    
-    menu.state('buyAirtime', {
-        run: () => {
-            menu.con('Enter amount:');
-        },
-        next: {
-            // using regex to match user input to next state
-            '*\\d+': 'buyAirtime.amount'
-        }
-    });
-    
-    // nesting states
-    menu.state('buyAirtime.amount', {
-        run: () => {
-            // use menu.val to access user input value
-            var amount = Number(menu.val);
-            buyAirtime(menu.args.phoneNumber, amount).then(function(res){
-                menu.end('Airtime bought successfully.');
-            });
-        }
-    });
+   
 
     // app.post('/africa-ussd', function(req, res){
     //     menu.run(req.body, ussdResult => {
@@ -333,7 +290,55 @@ module.exports = function() {
 
 
     app.post('/africa-ussd', (req, res) => {
-        console.log(menu)
+       
+
+
+        menu.startState({
+            run: () => {
+                // use menu.con() to send response without terminating session      
+                menu.con('Welcome. Choose option:' +
+                    '\n1. Show Balance' +
+                    '\n2. Buy Airtime');
+            },
+            // next object links to next state based on user input
+            next: {
+                '1': 'showBalance',
+                '2': 'buyAirtime'
+            }
+        });
+        
+        menu.state('showBalance', {
+            run: () => {
+                // fetch balance
+                fetchBalance(menu.args.phoneNumber).then(function(bal){
+                    // use menu.end() to send response and terminate session
+                    menu.end('Your balance is KES ' + bal);
+                });
+            }
+        });
+        
+        menu.state('buyAirtime', {
+            run: () => {
+                menu.con('Enter amount:');
+            },
+            next: {
+                // using regex to match user input to next state
+                '*\\d+': 'buyAirtime.amount'
+            }
+        });
+        
+        // nesting states
+        menu.state('buyAirtime.amount', {
+            run: () => {
+                // use menu.val to access user input value
+                var amount = Number(menu.val);
+                buyAirtime(menu.args.phoneNumber, amount).then(function(res){
+                    menu.end('Airtime bought successfully.');
+                });
+            }
+        });
+
+
         let args = {
             phoneNumber: req.body.phoneNumber,
             sessionId: req.body.sessionId,
