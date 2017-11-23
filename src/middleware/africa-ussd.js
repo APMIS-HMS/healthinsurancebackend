afb55f6eaeb5ea2ceda44caef5b00f4eab8bdc92
starday01 <sunday@apmis.ng>
2017-11-23 08:58:37 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -5,142 +5,57 @@ const options = {
 };
 const promise = [];
 
+menu.startState({
+    run: () => {
+        // use menu.con() to send response without terminating session      
+        menu.con('Welcome. Choose option:' +
+            '\n1. Show Balance' +
+            '\n2. Buy Airtime');
+    },
+    // next object links to next state based on user input
+    next: {
+        '1': 'showBalance',
+        '2': 'buyAirtime'
+    }
+});
+
+menu.state('showBalance', {
+    run: () => {
+        // fetch balance
+        fetchBalance(menu.args.phoneNumber).then(function (bal) {
+            // use menu.end() to send response and terminate session
+            menu.end('Your balance is KES ' + bal);
+        });
+    }
+});
+
+menu.state('buyAirtime', {
+    run: () => {
+        menu.con('Enter amount:');
+    },
+    next: {
+        // using regex to match user input to next state
+        '*\\d+': 'buyAirtime.amount'
+    }
+});
+
+// nesting states
+menu.state('buyAirtime.amount', {
+    run: () => {
+        // use menu.val to access user input value
+        var amount = Number(menu.val);
+        buyAirtime(menu.args.phoneNumber, amount).then(function (res) {
+            menu.end('Airtime bought successfully.');
+        });
+    }
+});
+
 const AfricasTalking = require('africastalking')(options);
 module.exports = function (app) {
     return function (req, res, next) {
-        var message = '';
-
-        var sessionId = req.body.sessionId;
-        var serviceCode = req.body.serviceCode;
-        var phoneNumber = req.body.phoneNumber;
-        var text = req.body.text;
-
-        console.log(sessionId, serviceCode, phoneNumber, text);
-
-        var length = text.split('*').length;
-        var txt = text.split('*');
-
-        if (text === '') {
-            message = 'CON Welcome to APMIS Health Insurance \n';
-            message += '1: Verify Registration \n';
-            message += '2: Register\n';
-            message += '3: Get Policy\n';
-            message += '4: Pay Premium';
-        }
-
-        // add device
-        else if (text === '1') {
-            // check if user is agent
-            console.log('am here');
-            message = 'CON Enter Policy ID';
-
-        }
-        else if (length === 2 && txt[0] === '1') {
-
-
-            promise.push(hook.app.service("policies")
-            .find({ query: { policyId: txt[1].toString()} }));
-            return Promise.all(promise).then(payload2 => {
-                console.log('fine')
-                message = 'END Policy checked';
-                var options = text.split('*');
-            }).catch(err =>{
-                console.log('error')
-                message = 'END Policy error';
-                var options = text.split('*');
-            })
-
-
-
-
-
-        }
-        else if (length === 3 && txt[0] === '1') {
-            message = 'CON Enter device model\n';
-            message += 'eg. Nokia 3310';
-        }
-        else if (length === 4 && txt[0] === '1') {
-            message = 'CON Enter Warranty status\n';
-            message += '1) Yes / 2) No';
-        }
-        else if (length === 5 && txt[0] === '1') {
-            message = 'CON Enter Insurance status\n';
-            message += '1). Yes / 2). No';
-        }
-        else if (length === 6 && txt[0] === '1') {
-            message = 'CON Is device in stock\n';
-            message += '1). Yes / 2). No';
-        }
-        else if (length === 7 && txt[0] === '1') {
-            // commit to db
-            message = 'END Device registered';
-            var options = text.split('*');
-
-        }
-
-        // add sales person
-        else if (text === '2') {
-            // check is user is agent
-            message = 'CON Enter sales agent name\n';
-        }
-        else if (length === 2 && txt[0] === '2') {
-            message = 'CON Enter sales agent email';
-        }
-        else if (length === 3 && txt[0] === '2') {
-            message = 'CON Enter Agent sales code';
-        }
-        else if (length === 4 && txt[0] === '2') {
-            message = 'CON Enter agent location\n';
-            message += 'eg. Kilimani';
-        }
-        else if (length === 5 && txt[0] === '2') {
-            message = 'END Sales agent added';
-            var options = text.split('*');
-
-        }
-
-
-        else if (text === '3') {
-            message = 'CON Enter device IMEI number';
-        }
-        else if (length === 2 && txt[0] === '3') {
-            message = 'CON Enter your ID number';
-        }
-        else if (length === 3 && txt[0] === '3') {
-            // check device authenticity
-            var options = text.split('*');
-
-            message = 'END You do not have a genuine device';
-        }
-
-
-        else if (text === '4') {
-            message = 'CON Enter Sales code';
-        }
-        else if (length === 2 && txt[0] === '4') {
-            message = 'CON Enter Phone IMEI number';
-        }
-        else if (length === 3 && txt[0] === '4') {
-            message = 'CON Enter Buyer ID number';
-        }
-        else if (length === 4 && txt[0] === '4') {
-            message = 'CON Enter Buyer Name';
-        }
-        else if (length === 5 && txt[0] === '4') {
-            message = 'CON Enter Buyer Phone number';
-        }
-        else if (length === 6 && txt[0] === '4') {
-            message = 'END Device sold';
-            var options = text.split('*');
-        }
-        else {
-            message = 'END Wrong input';
-            // reply with menu
-        }
-
-        res.contentType('text/plain');
-        // res.send(message, 200);
-        res.status(200).send(message)
+        menu.run(req.body, ussdResult => {
+            res.send(ussdResult);
+        });
 
     };
 }
