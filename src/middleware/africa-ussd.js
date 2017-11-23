4555e23f69ad86af29cf89a1e94949b0e1586477
starday01 <sunday@apmis.ng>
2017-11-23 07:03:55 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -35,7 +35,9 @@ module.exports = function (app) {
         
         }
         else if (length === 2 && txt[0] === '1') {
-            message = 'CON You are good';
+            message = 'CON You '+ txt[1];
+            // message = 'END Device registered';
+            // var options = text.split('*');
         }
         else if (length === 3 && txt[0] === '1') {
             message = 'CON Enter device model\n';
