2759455b6f86c392e1b18da47db09ae97394557a
starday01 <sunday@apmis.ng>
2017-11-23 07:59:58 +0100

    updates

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -111,7 +111,7 @@ module.exports = function() {
     app.get('/api/search-policy', searchPolicy(app));
     app.get('/api/compare-password', compare_password(app));
     app.get('/api/get-beneficiary-count', get_beneficiary_count(app));
-    app.post('/api/africa-ussd', africa_ussd(app));
+    // app.post('/api/africa-ussd', africa_ussd(app));
 
     //-------------USSD API PLUG START-------------------
     app.get('/api/ussd/verify-user', user_verification(app));
@@ -273,6 +273,61 @@ module.exports = function() {
                     err_desc: "Corupted excel file"
                 });
             }
+        });
+    });
+
+    const UssdMenu = require('ussd-menu-builder');
+    let menu = new UssdMenu();
+
+
+    menu.startState({
+        run: () => {
+            // use menu.con() to send response without terminating session      
+            menu.con('Welcome. Choose option:' +
+                '\n1. Show Balance' +
+                '\n2. Buy Airtime');
+        },
+        // next object links to next state based on user input
+        next: {
+            '1': 'showBalance',
+            '2': 'buyAirtime'
+        }
+    });
+    
+    menu.state('showBalance', {
+        run: () => {
+            // fetch balance
+            fetchBalance(menu.args.phoneNumber).then(function(bal){
+                // use menu.end() to send response and terminate session
+                menu.end('Your balance is KES ' + bal);
+            });
+        }
+    });
+    
+    menu.state('buyAirtime', {
+        run: () => {
+            menu.con('Enter amount:');
+        },
+        next: {
+            // using regex to match user input to next state
+            '*\\d+': 'buyAirtime.amount'
+        }
+    });
+    
+    // nesting states
+    menu.state('buyAirtime.amount', {
+        run: () => {
+            // use menu.val to access user input value
+            var amount = Number(menu.val);
+            buyAirtime(menu.args.phoneNumber, amount).then(function(res){
+                menu.end('Airtime bought successfully.');
+            });
+        }
+    });
+
+    app.post('/africa-ussd', function(req, res){
+        menu.run(req.body, ussdResult => {
+            res.send(ussdResult);
         });
     });
 
