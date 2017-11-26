11f26ebf9c5d5258726d7cde63b1f4e7dea8afe3
starday01 <sunday@apmis.ng>
2017-11-26 23:07:21 +0100

    updates

--- src/middleware/africa-ussd.js
+++ src/middleware/africa-ussd.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 const options = {
     apiKey: '2d4e0c15346ba6e810bcfd74ed300cb4042a000fba6effeadbcb775e538bce44',         // Use sandbox API key for sandbox development
     username: 'sandbox',      // Use "sandbox" for sandbox development
