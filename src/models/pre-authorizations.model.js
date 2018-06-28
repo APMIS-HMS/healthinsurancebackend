92760929edfa3daaf767d3df01e6fbfca837f56c
Sunday Orimoyegun <sunday.orimoyegun@apmis.ng>
2018-06-28 15:33:03 +0100

    updates to pre authorization model with platformownerid

--- src/models/pre-authorizations.model.js
+++ src/models/pre-authorizations.model.js
@@ -1,35 +1,36 @@
 // pre-authorizations-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
-  const claimStatusModel = require('../viewmodels/claim-status-model');
-  const claimAlertMessage = require('../viewmodels/claim-message-model');
-  const preAuthDoc = require('../viewmodels/pre-authorization-model');
-  const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const preAuthorizations = new Schema({
-    providerFacilityId: { type: Schema.Types.ObjectId, required: true },
-    checkedInDetails: { type: Schema.Types.ObjectId, required: true },
-    policyId: { type: Schema.Types.ObjectId, required: true },
-    personId: { type: Schema.Types.ObjectId, required: true },
-    principalBeneficiaryId: { type: Schema.Types.ObjectId, required: true },
-    beneficiaryId: { type: Schema.Types.ObjectId, required: true },
-    documentation: [preAuthDoc],
-    approval: { type: Schema.Types.Date, required: false },
-    medicalPersonelName: { type: Schema.Types.Mixed, required: true },
-    medicalPersonelUnit: { type: String, required: true },
-    authorizationCode: { type: String, required: false },
-    approvedDocumentation: [preAuthDoc],
-    notificationMessage: claimAlertMessage,
-    dateOfRequest: { type: Date },
-    timeOfRequest: { type: Date, default: Date.now },
-    isEmergency: { type: Schema.Types.Boolean, 'default':false},
-    visityClassId: { type: Schema.Types.Mixed, required:true},
-    approvedStatus: { type: Schema.Types.Mixed, require: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
-  });
+module.exports = function(app) {
+    const claimStatusModel = require('../viewmodels/claim-status-model');
+    const claimAlertMessage = require('../viewmodels/claim-message-model');
+    const preAuthDoc = require('../viewmodels/pre-authorization-model');
+    const mongooseClient = app.get('mongooseClient');
+    const { Schema } = mongooseClient;
+    const preAuthorizations = new Schema({
+        providerFacilityId: { type: Schema.Types.ObjectId, required: true },
+        checkedInDetails: { type: Schema.Types.ObjectId, required: true },
+        policyId: { type: Schema.Types.ObjectId, required: true },
+        personId: { type: Schema.Types.ObjectId, required: true },
+        principalBeneficiaryId: { type: Schema.Types.ObjectId, required: true },
+        beneficiaryId: { type: Schema.Types.ObjectId, required: true },
+        documentation: [preAuthDoc],
+        approval: { type: Schema.Types.Date, required: false },
+        medicalPersonelName: { type: Schema.Types.Mixed, required: true },
+        medicalPersonelUnit: { type: String, required: true },
+        authorizationCode: { type: String, required: false },
+        approvedDocumentation: [preAuthDoc],
+        notificationMessage: claimAlertMessage,
+        dateOfRequest: { type: Date },
+        timeOfRequest: { type: Date, default: Date.now },
+        isEmergency: { type: Schema.Types.Boolean, 'default': false },
+        visityClassId: { type: Schema.Types.Mixed, required: true },
+        approvedStatus: { type: Schema.Types.Mixed, require: true },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
+        platformOwnerId: { type: Schema.Types.ObjectId, required: false }
+    });
 
-  return mongooseClient.model('preAuthorizations', preAuthorizations);
+    return mongooseClient.model('preAuthorizations', preAuthorizations);
 };
