74a6886006bab31cf71caaf15a2f51b5fcd6bd43
Chisimdi Damian Ezeanieto <chisimdi.ezeanieto@apmis.ng>
2018-08-02 22:52:30 +0100

    Updated.

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -112,11 +112,12 @@ module.exports = function() {
     app.post('/api/beneficiaries', create_beneficiary_api(app));
     app.post('/api/send-sms', sendSMS(app));
     app.get('/api/validate-age', validateAge(app));
+    // I changed search-policy to post because i needed to use filter the data for the platformowner
     app.post('/api/search-policy', searchPolicy(app));
-    app.post('/api/crud-role', crudRole(app));
     app.get('/api/compare-password', compare_password(app));
     app.get('/api/get-beneficiary-count', get_beneficiary_count(app));
     app.post('/api/africa-ussd', africa_ussd(app));
+    app.post('/api/crud-role', crudRole(app));
 
     //-------------USSD API PLUG START-------------------
     app.get('/api/ussd/verify-user', user_verification(app));
