3d2f1e56922de526631d9d2e8a304ff6ffc3f634
Chisimdi Damian Ezeanieto <chisimdi.ezeanieto@apmis.ng>
2018-08-15 14:17:20 +0100

    Updated pagination.

--- src/hooks/send-policy-sms-notice.js
+++ src/hooks/send-policy-sms-notice.js
@@ -5,20 +5,19 @@ module.exports = function(options = {}) { // eslint-disable-line no-unused-vars
     return function sendPolicySmsNotice(hook) {
         // Hooks can either return nothing or a promise
         // that resolves with the `hook` object for asynchronous operations
-        let numbers = hook.data.principalBeneficiary.personId.phoneNumber;
-        let sender = hook.data.platformOwnerId.shortName;
-        let policyOwner = hook.data.hiaId != undefined ?
-            hook.data.hiaId :
-            hook.data.platformOwnerId.shortName;
-        let message = policyOwner +
-            ' created an Health Insurance Policy on your behalf with PolicyID:' +
-            hook.data.policyId + '. Your ' + sender + '-ID is ' +
-            hook.data.principalBeneficiary.platformOwnerNumber + '. Thanks';
-
-        const url =
-            'http://portal.bulksmsnigeria.net/api/?username=apmis&password=apmis&message=' +
-            message + '&sender=' + sender + '&mobiles=@@' + numbers + '@@';
-        var response = request.get(url);
+        const numbers = hook.data.principalBeneficiary.personId.phoneNumber;
+        console.log('Number ', numbers);
+        const sender = hook.data.platformOwnerId.shortName;
+        console.log('Sender ', sender);
+        const policyOwner = hook.data.hiaId != undefined ? hook.data.hiaId : hook.data.platformOwnerId.shortName;
+        console.log('PolicyOwner ', policyOwner);
+        const message = `${policyOwner} created an Health Insurance Policy on your behalf with PolicyID: ${hook.data.policyId}.
+         Your ${sender}-ID ${hook.data.principalBeneficiary.platformOwnerNumber}. Thanks`;
+        console.log('Message ', message);
+        const url = `http://portal.bulksmsnigeria.net/api/?username=apmis&password=apmis&message=${message}&sender=${sender}&mobiles=@@${numbers}@@`;
+        console.log('Url ', url);
+        const response = request.get(url);
+        console.log(response);
         return Promise.resolve(hook);
     };
 };
